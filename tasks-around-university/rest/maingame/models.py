8a569b9cf1a33a330760c37184bec8456d4de520
Joni Pesonen <jonimikaelpesonen@hotmail.com>
2018-10-25 18:14:00 +0300

    Creation of database models and basic API function.

--- tasks-around-university/rest/maingame/models.py
+++ tasks-around-university/rest/maingame/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Hotspot(models.Model):
+    id = models.AutoField(primary_key=True)
+    x = models.IntegerField()
+    y = models.IntegerField()
+    #created_at: models.DateField(auto_now_add=True)
+    #updated_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(default=timezone.now)
+    configuration = models.CharField(max_length=1024, blank=True)
+    qrCode = models.CharField(max_length=1024, blank=True)
+    
+    #date päivän tarkkuus datetime mikrosekunti? default=timezone.now pitää muokattavuuden
+
+class Player(models.Model):
+    id = models.AutoField(primary_key=True)
+    x = models.IntegerField()
+    y = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(default=timezone.now)
+    # nimi, opintosuuntaus?
+
+class Group(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+class Player_Group(models.Model):
+    group = models.ForeignKey(Group, related_name='related_group', on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, related_name='related_player', on_delete=models.CASCADE)
+    isLeader = models.BooleanField()
