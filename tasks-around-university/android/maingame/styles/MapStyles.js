0b6f4dc83c6685ad5e3269dc7e2fbfb9ff65a166
jerehuu <huumonen.jere.j@student.uta.fi>
2018-11-09 20:27:58 +0200

    Map component added

---
+++ tasks-around-university/android/maingame/styles/MapStyles.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    map: {
+        width: '100%',
+        height: '100%'
+    }
+});
+
+export default styles;
