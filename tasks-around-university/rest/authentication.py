9454d1730bd230f452650b1ef9a073ad770e2712
JaliJuhola <jalijuhola@gmail.com>
2018-12-10 21:49:48 +0000

    debug

--- tasks-around-university/rest/authentication.py
+++ tasks-around-university/rest/authentication.py
@@ -6,14 +6,11 @@ class TokenAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
         if "api/auth" not in request.path:
             player_token = request.META.get('HTTP_AUTHORIZATION')
-            print(type(player_token))
-            print("auth: " + player_token)
             if not player_token:
                 print("no token")
                 raise exceptions.AuthenticationFailed('No authentication token given!')
             try:
                 player = Player.objects.filter(token=str(player_token))
-                print(player.__dict__)
             except(Player.DoesNotExist):
                 print("no such user")
                 raise exceptions.AuthenticationFailed('No such user')
