dc582215dfec83e5d533dd23d5281823758ab2a6
JaliJuhola <jalijuhola@gmail.com>
2018-12-10 21:04:39 +0000

    debug

--- tasks-around-university/rest/authentication.py
+++ tasks-around-university/rest/authentication.py
@@ -1,6 +1,6 @@
 from rest_framework import authentication
 from rest_framework import exceptions
-from rest.maingame.models import Player
+from rest.maingame.models import Player, Group
 
 class TokenAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
@@ -8,8 +8,10 @@ class TokenAuthentication(authentication.BaseAuthentication):
             player_token = request.META.get('HTTP_AUTHORIZATION')
             print("auth: " + player_token)
             if not player_token:
+                print("no token")
                 raise exceptions.AuthenticationFailed('No authentication token given!')
             try:
+                print("player objects.get")
                 player = Player.objects.get(token=player_token)
                 print(player.__dict__)
             except(Player.DoesNotExist):
