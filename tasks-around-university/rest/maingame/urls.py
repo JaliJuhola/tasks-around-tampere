5c675d5e35d6d228a2cd79f04a2a35986ead7f38
JaliJuhola <jalijuhola@gmail.com>
2018-12-15 11:02:37 +0000

    uus

--- tasks-around-university/rest/maingame/urls.py
+++ tasks-around-university/rest/maingame/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('api/group/player', views.PlayerGroupView.as_view()),
     # path('pusher/auth', views.pusher_authentication),
     path('api/push_the_buttons', PushTheButtonView.as_view()),
-    path('api/me', views.PlayerView.as_view())
+    path('api/me', views.PlayerView.as_view()),
     path('api/group/create', views.GroupView.as_view())
 ]
