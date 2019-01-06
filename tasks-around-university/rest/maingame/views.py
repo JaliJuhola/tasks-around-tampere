51ebb7a93cbea0a10edf9e10d5a94a6bbfb7daf1
JaliJuhola <jalijuhola@gmail.com>
2019-01-06 01:19:51 +0000

    user types for map

--- tasks-around-university/rest/maingame/views.py
+++ tasks-around-university/rest/maingame/views.py
@@ -183,7 +183,7 @@ class PlayerLocationView(APIView):
             player_type = 1
             if player == request.user:
                 player_type= 3
-            else if player.leader:
+            elif player.leader:
                 player_type = 2
             response_array.append({'name': player.name, 'type': player_type 'location': {'longitude': player.x, 'latitude': player.y}, 'avatar': "../assets/testmarker.png"})
         return Response({'players': response_array})
